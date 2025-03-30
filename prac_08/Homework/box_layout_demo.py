6030901a0eb327049812e6e3c0e5fadaae6db9b7
Vince <vincelin126@gmail.com>
2025-03-30 14:23:51 +0800

    Prac 8

--- prac_08/Homework/box_layout_demo.py
+++ prac_08/Homework/box_layout_demo.py
@@ -1,12 +1,20 @@
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 
+class BoxLayoutDemo(BoxLayout):
+    pass
 
-class BoxLayoutDemo(App):
+class BoxLayoutApp(App):
     def build(self):
-        self.title = "Box Layout Demo"
-        self.root = Builder.load_file('box_layout.kv')
-        return self.root
+        return BoxLayoutDemo()
 
+    def handle_greet(self):
+        print("greet")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
-BoxLayoutDemo().run()
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
+if __name__ == '__main__':
+    BoxLayoutApp().run()
