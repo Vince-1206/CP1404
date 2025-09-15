34dd86e759569449c4d19f0d53d2043046bf9c4c
Vince <vincelin126@gmail.com>
2025-09-15 13:54:21 +0800

    this is my work

--- prac_01/Week 1 Practical/Broken_Score.py
+++ prac_01/Week 1 Practical/Broken_Score.py
@@ -1,7 +1,7 @@
 """
 CP1404/CP5632 - Practical
 Broken program to determine score status
-"""
+
 
 # TODO: Fix this!
 
@@ -17,3 +17,28 @@ else:
     print("Excellent")
 if score < 50:
     print("Bad")
+"""
+
+"""
+fixed_score.py - corrected score checker
+
+Rules:
+ - score must be between 0 and 100 inclusive
+ - 90 or more -> Excellent
+ - 50 or more -> Passable
+ - below 50 -> Bad
+"""
+
+def classify_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+if __name__ == "__main__":
+    score = float(input("Enter score: "))
+    print(classify_score(score))
